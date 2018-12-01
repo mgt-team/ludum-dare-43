638a05323998ecefdd1b0cdde7a0396460622837
Max Tushkevich <maxonchik174@gmail.com>
2018-12-01 21:45:49 +0300

    Add CommonDamageEffect

---
+++ Assets/Scripts/Common/Live/Damage/CommonDamageEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonDamageEffect : DamageEffect
+{
+    public override void OnDamageReceived(DamageInfo damageInfo)
+    {
+        DamageForce(damageInfo); 
+    }
+
+    private void DamageForce(DamageInfo damageInfo)
+    {
+        transform.position = Vector2.Lerp(transform.position, damageInfo.AssaulterPosition, Time.deltaTime * damageInfo.DamageCount);
+    }
+}
