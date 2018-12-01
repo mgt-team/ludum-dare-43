a23e2a9cdc34c11e6bcd4f7cbdf3a054f68ba3ba
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-01 15:46:02 +0300

    Add scripts for tracking live status

---
+++ Assets/Scripts/Common/Live/Liveble.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Dieble))]
+public class Liveble : MonoBehaviour
+{
+	public HpConfig HpConfig;
+	private int _currentHp;
+	private Dieble _dieble;
+
+	private bool _isAlive = true;
+
+	public bool IsAlive()
+	{
+		return _isAlive;
+	}
+
+	public int GetCurrentHp()
+	{
+		return _currentHp;
+	}
+
+	private void Awake()
+	{
+		_dieble = GetComponent<Dieble>();
+	}
+
+	public void InitHp()
+	{
+		_currentHp = HpConfig.Hp;
+		_isAlive = true;
+	}
+
+	public void IncreaseHp(int hp)
+	{
+		_currentHp = Mathf.Clamp(_currentHp + hp, 0, HpConfig.Hp);
+		OnHpUpdated();
+	}
+		
+	public void DecreaseHp(int hp)
+	{
+		_currentHp = Mathf.Clamp(_currentHp - hp, 0, HpConfig.Hp);
+		OnHpUpdated();
+	}
+
+	private void OnHpUpdated()
+	{
+		CheckHp();
+	}
+
+	private void CheckHp()
+	{
+		if (_currentHp <= 0)
+		{
+			_isAlive = false;
+			_dieble.Die();
+		}
+	}
+}
