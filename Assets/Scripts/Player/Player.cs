a21bc6aa67f824a1bc5429d841411eb797565ca4
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-01 11:28:29 +0300

    Add Player scripts

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour {
+	
+	[SerializeField]
+	private Vector2 _shiftPower;	
+
+	private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void StopAnimation()
+    {
+        _animator.SetBool("isRun", false);
+    }
+
+    public void StartAnimation()
+    {
+        _animator.SetBool("isRun", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	public void SetVelocity(Vector2 direction)
+	{
+		if (direction.x != 0 || direction.y != 0)
+			direction *= Mathf.Sqrt(2) / 2;
+
+		var shiftPowerY = _shiftPower.y;
+
+		if(direction.y < 0)
+		{
+			shiftPowerY *= 2;
+		}
+		else
+		{
+			shiftPowerY *= 1.5f;
+		}
+		
+		_rigidbody.velocity = new Vector3(direction.x * _shiftPower.x, direction.y * shiftPowerY);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Player _player;
+    
+    private MoveController _moveController;
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _moveController = GetComponent<MoveController>();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayerMovement();
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        Vector2 velocity = _moveController.GetVelocity();
+        _player.SetVelocity(velocity);
+        /*if (velocity == Vector2.zero)
+            _player.StopAnimation();
+        else
+            _player.StartAnimation();*/
+    }
+}
