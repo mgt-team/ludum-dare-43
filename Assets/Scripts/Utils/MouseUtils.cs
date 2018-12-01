9dc8f3af1b484732a53c226b14b28fc06dbdee13
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-01 12:18:36 +0300

    Add weapon movement

---
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class MouseUtils {
+    
+    public static float GetMouseAndTargetAngle(Vector3 targetPosition)
+    {
+        return MathUtils.GetPointOnTargetLookAngle(targetPosition, GetMousePosition());
+    }
+
+    public static Vector3 GetMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.transform.position.z));
+    }
+}
