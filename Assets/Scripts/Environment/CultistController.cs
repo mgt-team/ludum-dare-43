cf4adf7d331f36859ce1d870d6a7272448b58a9d
Max Tushkevich <maxonchik174@gmail.com>
2018-12-01 15:16:15 +0300

    Add CultistController

---
+++ Assets/Scripts/Environment/CultistController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _fightDistance;
+
+    [SerializeField]
+    private float _speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector2.Distance(transform.position, _target.position);
+        if (distance > _fightDistance)
+            transform.position = Vector2.Lerp(transform.position, _target.position, Time.deltaTime * _speed);
+        else
+            MakeSacrifice();
+	}
+
+    private void MakeSacrifice()
+    {
+
+    }
+}
