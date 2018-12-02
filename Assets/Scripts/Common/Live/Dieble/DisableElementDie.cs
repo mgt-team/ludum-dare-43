e055b53a69beaf8face12467662d2cb47336e5d1
Max Tushkevich <maxonchik174@gmail.com>
2018-12-02 14:37:49 +0300

    Add PlayerDeath Animation

--- Assets/Scripts/Common/Live/Dieble/DisableElementDie.cs
+++ Assets/Scripts/Common/Live/Dieble/DisableElementDie.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class DisableElementDie : Dieble {
 
+    [SerializeField]
+    private float _animationTimer;
+    private bool _isDie;
+    private float _timer;
+
 	public override void Die()
 	{
-		gameObject.SetActive(false);
+        _timer = _animationTimer;
+        _isDie = false;
+		//gameObject.SetActive(false);
 	}
+
+    private void Update()
+    {
+        if(_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            _isDie = false;
+        }
+        if(_timer < 0)
+        {
+            _isDie = true;
+        }
+        if (_timer < 0 && _isDie)
+            Destroy(gameObject);
+    }
 }
