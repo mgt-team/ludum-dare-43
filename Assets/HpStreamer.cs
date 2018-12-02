2ebc258c25a6f2ddb44479c19420ca0e39e46a83
Max Tushkevich <maxonchik174@gmail.com>
2018-12-02 15:45:07 +0300

    Add HPStreamer

---
+++ Assets/HpStreamer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpStreamer : MonoBehaviour {
+
+    [SerializeField]
+    private Liveble _player;
+
+    private Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        _text.text = "HP:" + _player.CurrentHp.ToString();
+    }
+}
